8e6369a9fdd0291a488320a350709199df0ef385
theServerGod <the.server.god@gmail.com>
2016-03-26 15:06:34 +1000

    Updated gulp common lib

--- gulp/common.lib.js
+++ gulp/common.lib.js
@@ -13,7 +13,7 @@
 		var difference = (data.savings > 0) ? ' smaller.' : ' larger.';
 		return data.fileName + ' went from ' +
 			(data.startSize / 1000).toFixed(2) + ' kB to ' + (data.endSize / 1000).toFixed(2) + ' kB' +
-			' and is ' + formatPercent(1 - data.percent, 2) + '%' + difference;
+			' and is ' + common.formatPercent(1 - data.percent, 2) + '%' + difference;
 	}
 
 	/**
